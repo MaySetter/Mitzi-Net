599b5ca680a08ff64579e0389d3c2dafac05e81f
MaySetter <maysetter@gmail.com>
2024-06-24 23:52:20 +0300

    commit point 24/06/24

---
+++ server.js
@@ -0,0 +1,140 @@
+/**
+ * Assignment 2
+ * GitHub: https://github.com/MaySetter/Mitzi-Net
+ * Submitted by:
+ * Student 1. May Seter  ID# 312123037
+ * Student 2. Dmitry Ignatiev  ID# 316810910
+ **/
+
+const express = require('express');
+const mongojs = require('mongojs');
+const bcrypt = require('bcryptjs');
+
+// our specific collection
+const db = mongojs('mongodb+srv://Student:webdev2024student@cluster0.uqyflra.mongodb.net/webdev2024', ['Dima']);
+const clients_coll = db.collection('mitzinet_<Dima_May>');
+
+const app = express();
+app.use(express.json());
+
+// Serve static files from the 'static' directory
+app.use(express.static('static'));
+
+// Route to get all clients
+app.get('/clients', (req, res) => {
+    clients_coll.find({}, (err, clients) => {
+        if (err) {
+            console.error('Error fetching clients:', err);
+            return res.status(500).json({ error: 'Server error' });
+        }
+        res.json(clients);
+    });
+});
+
+// validates form fields and inserts to collection
+app.post('/register', (req, res) => {
+    console.log("Registration data:", req.body);
+    const { firstName, lastName, phoneNumber, email, password, passwordConfirm } = req.body;
+    // validates all fields using validation function
+    const valid_error = validation(req.body);
+    // if valid_error isn't null, return status 400 with the text message turned to json
+    if (valid_error) {
+        return res.status(400).json({ error: valid_error });
+    }
+    // checks if email isn't already in the database
+    clients_coll.findOne({ email }, (err, client) => {
+        if (err) {
+            console.error('Error checking existing user:', err);
+            return res.status(500).json({ error: 'Server error' });
+        }
+        // if client exists, returns error
+        if (client) {
+            return res.status(400).json({ error: 'User already exists' });
+        }
+        // if client with the same email does not exist in database, hash the password
+        bcrypt.hash(password, 8, (err, hashedPassword) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Server error' });
+            }
+            // creates new client with hashed password
+            const newClient = {
+                firstName,
+                lastName,
+                phoneNumber,
+                email,
+                password: hashedPassword
+            };
+            // inserts the new client to the database
+            clients_coll.insert(newClient, (err, result) => {
+                if (err) {
+                    console.error('Error inserting user:', err);
+                    return res.status(500).json({ error: 'Internal server error' });
+                }
+                res.status(201).json({ message: 'Registration successful' });
+            });
+        });
+    });
+});
+
+// function to check the fields' validation. if there's error, will return text message.
+// otherwise will return null.
+function validation(doc){
+    const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!doc.firstName || !doc.lastName || !doc.email || !doc.password || !doc.passwordConfirm) {
+        return "כל השדות חייבים להיות מלאים.";
+    }
+    if (doc.firstName.trim().length === 0) {
+        return "שם לא חוקי. מותרים רק אותיות ורווחים";
+    }
+    if (doc.lastName.trim().length === 0) {
+        return "שם לא חוקי. מותרים רק אותיות ורווחים";
+    }
+    if (!email_pattern.test(doc.email)) {
+        return "כתובית אימייל לא חוקית.";
+    }
+    if (doc.password.trim().length < 8) {
+        return "סיסמה - לפחות 8 תווים";
+    }
+    if (doc.password !== doc.passwordConfirm) {
+        return "שדה ״וידוא סיסמה״ חייב להיות זהה לשדה סיסמה";
+    }
+    return null;
+};
+
+// deletes
+app.delete('/clients', (req, res) => {
+    const { email, password } = req.body;
+    // finds the first client with the same email
+    clients_coll.findOne({ email:email }, (err, client) => {
+        if (err) {
+            return res.status(500).json({ error: 'Server error' });
+        }
+        // if client doesn't exist return 404 error
+        if (!client) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        // return 0 if the password is the same as the hashed password
+        bcrypt.compare(password, client.password, (err, match) => {
+            if (err) {
+                return res.status(500).json({ error: 'Server error' });
+            }
+            // client with same email found, but password doesn't match
+            if (!match) {
+                return res.status(400).json({ error: 'Incorrect password' });
+            }
+            // client with same email and password - remove from database
+            clients_coll.remove({ email: email }, (err, result) => {
+                if (err) {
+                    return res.status(500).json({ error: 'Server error' });
+                }
+                res.status(201).json({ message: 'User deleted successfully' });
+            });
+        });
+    });
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
---
+++ static/script.js
@@ -0,0 +1,153 @@
+﻿/**
+ * Assignment 2
+ * GitHub: https://github.com/MaySetter/Mitzi-Net
+ * Submitted by:
+ * Student 1. May Seter  ID# 312123037
+ * Student 2. Dmitry Ignatiev  ID# 316810910
+ **/
+
+// event listener for when submit button is clicked, activate register_clicked() function
+document.getElementById('registrationForm').addEventListener('submit', register_clicked);
+// event listener for when delete button is clicked, activate deleteUser() function
+document.getElementById('deleteButton').addEventListener('click', deleteUser);
+
+let register_success;
+/**
+ * Function will check for each input element if input is valid.
+ * If so, will alert that registration has been successful.
+ * @param event
+ */
+function register_clicked(event) {
+    register_success = true;
+    event.preventDefault();
+
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let phoneNumber = document.getElementById("phoneNumber").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let passwordConfirm = document.getElementById("passwordConfirm").value;
+
+    const name_pattern = /^[(A-Za-z)|\u0590-\u05FF\s]+$/;
+    const phone_pattern = /^[0-9]{10}$/;
+    const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    const name_text = "שם לא חוקי. מותרים רק אותיות ורווחים";
+    const phone_text = "מספר לא חוקי. נדרשות 10 ספרות ללא תווים";
+    const email_text = "כתובית אימייל לא חוקית.";
+    const password_text = "סיסמה - לפחות 8 תווים";
+    const password_confirm_text = "שדה ״וידוא סיסמה״ חייב להיות זהה לשדה סיסמה";
+
+    if (firstName !== "" && !name_pattern.test(firstName)) {
+        errorProblem("firstName", name_text, "name_error");
+    } else if (lastName !== "" && !name_pattern.test(lastName)) {
+        errorProblem("lastName", name_text, "name_error");
+    }
+    if (phoneNumber !== "" && !phone_pattern.test(phoneNumber)) {
+        errorProblem("phoneNumber", phone_text, "tel_error");
+    }
+    if (email === "" || !email_pattern.test(email)) {
+        errorProblem("email", email_text, "email_error");
+    }
+    if (password.length < 8) {
+        errorProblem("password", password_text, "password_error");
+    }
+    if (passwordConfirm !== password) {
+        errorProblem("passwordConfirm", password_confirm_text, "confirm_error");
+    }
+
+    if (register_success) {
+        sendRegistrationData(firstName, lastName, phoneNumber, email, password, passwordConfirm);
+    }
+}
+
+/**
+ * Function assigns register_success as false.
+ * Gets a <p> element by an error id and insert error message text to element.
+ * Changes the problem element's border to red. When user clicks on problem element,
+ * function deletes message and turns the border back to normal.
+ * @param problemId HTML element
+ * @param text Error message
+ * @param errorId Error id
+ */
+function errorProblem(problemId, text, errorId) {
+    register_success = false;
+    let textElement = document.getElementById(errorId);
+    textElement.innerHTML = text;
+    let problem = document.getElementById(problemId);
+    problem.style.border = "1px solid red";
+    problem.addEventListener("click", function () {
+        textElement.innerHTML = "";
+        problem.style.border = "";
+    });
+}
+
+/**
+ * fetch POST. turns all the parameters to a json object
+ */
+function sendRegistrationData(firstName, lastName, phoneNumber, email, password, passwordConfirm) {
+    fetch('http://localhost:3000/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            firstName: firstName,
+            lastName: lastName,
+            phoneNumber: phoneNumber,
+            email: email,
+            password: password,
+            passwordConfirm: passwordConfirm
+        })
+    })
+        .then((response) => {
+            if (!response.ok) {
+                return response.json().then((data) => {
+                    throw new Error(data.error);
+                });
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            location.reload(); // refresh page after
+            alert("ההרשמה בוצעה בהצלחה");
+        })
+        .catch(error => {
+            console.error('Error during registration:', error);
+            alert(`Registration error: ${error.message}`);
+        });
+}
+
+function deleteUser() {
+    const email = document.getElementById("delete_email").value;
+    const password = document.getElementById("delete_password").value;
+
+    fetch('http://localhost:3000/clients', {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password
+        })
+    })
+        .then(response => {
+            if (!response.ok) {
+                return response.json().then((data) => {
+                    throw new Error(data.error);
+                });
+            }
+            return response.json();
+        })
+        .then(data => {
+            location.reload(); // refresh page after
+            alert("משתמש נמחק בהצלחה");
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error during deletion:', error);
+            alert(`Deletion error: ${error.message}`);
+        });
+}
